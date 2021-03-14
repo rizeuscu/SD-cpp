a9285b62b69f81134271c5f789e58d70df3f4c90
Bogdan-Constantin Rizescu <rizescubogdan2001@gmail.com>
2021-03-14 16:48:31 +0200

    Added solution for lab1

--- lab_1_schelet/Fraction.h
+++ lab_1_schelet/Fraction.h
@@ -9,26 +9,39 @@ private:
 public:
 	Fraction(double num, double denom) {
 		// TODO constructor that initializes fields
+		this->num = num;
+		this->denom = denom;
 	}
 
 	double getNum() {
 		// TODO get private numerator
+		return this->num;
 	}
 
 	double getDenom() {
 		// TODO get private denominator
+		return this->denom;
 	}
 
 	double decimal() {
 		// TODO compute decimal equivalent to fraction
+		return this->num / this->denom;
 	}
 
 	Fraction multiply(Fraction f) {
 		// TODO compute product of this and f
+		Fraction result(f.num, f.denom);
+		result.num *= this->num;
+		result.denom *= this->denom;
+		return result;
 	}
 
 	Fraction divide(Fraction f) {
 		// TODO compute division of this and f
+		Fraction result(this->num, this->denom);
+		result.num *= f.denom;
+		result.denom *= f.num;
+		return result;
 	}
 };
 
--- lab_1_schelet/Gradebook.h
+++ lab_1_schelet/Gradebook.h
@@ -10,18 +10,31 @@ private:
 public:
 	Gradebook(int capacity) {
 		// TODO initialize grades, capacity and count
+		this->grades = new double[capacity];
+		this->capacity = capacity;
+		this->count = 0;
 	}
 
 	~Gradebook() {
 		// TODO free memory
+		delete[] this->grades;
 	}
 
 	void addGrade(double grade) {
 		// TODO add new grade to grades array (if it reaches its capacity return)
+		if (this->count == this->capacity)
+			return;
+		this->grades[this->count] = grade;
+		this->count++;
 	}
 
 	double average() {
 		// TODO compute average of the grades that have been added
+		double sum = 0;
+		for (int i = 0; i < this->count; ++i) {
+			sum += this->grades[i];
+		}
+		return sum / this->count;
 	}
 };
 
